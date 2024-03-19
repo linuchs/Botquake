2ed5b8d2dfb8b80c134d2b7ac5b5ed5aed2e130e
linuchs <allegrarex#libero.it>
2024-03-19 12:46:32 +0100

    Modifica la struttura del progetto

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+"""Aggiunge la cartella src al path di Python per permettere l'importazione dei moduli."""
+import sys
+
+sys.path.append(".")
+sys.path.append("./src")
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,14 +4,12 @@ from datetime import date, timedelta
 from pytest_mock import MockerFixture
 import pytest
 from telegram.ext import Application
-from utils.helper.gethelp import generate_url, get_date_range
-from utils.helper.classes import ZoneMap
-from data.testuale import TESTO_01
-from data.testuale import MENU
-from data.testuale import BENVENUTO
-import bot_quake
-
-from bot_quake import info, file_reader, start, handle_message # pylint: disable=unused-import
+from src.gethelp import generate_url, get_date_range
+from src.classes import ZoneMap
+from src.testuale import TESTO_01
+from src.testuale import MENU
+from src.testuale import BENVENUTO
+from src import bot_quake  # pylint: disable=unused-import
 
 
 
